01d11109c1fce4a9180a3f3e6cbe8a9af9806494
Javier Cavalli <javier.cavalli@gmail.com>
2023-03-09 18:38:40 -0300

    added checkbox filtering 09032022

--- index.js
+++
@@ -1,6 +0,0 @@
-const navbarToggle = document.querySelector(".navbar-toggle");
-const navbarLinksToggle = document.querySelector(".navbar-links-toggle");
-
-navbarToggle.addEventListener("click", () => {
-  navbarLinksToggle.classList.toggle("active");
-});
